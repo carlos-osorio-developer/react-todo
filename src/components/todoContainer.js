6485dccf9b907b25ef8c5048a850060b2cd1838f
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-20 13:25:49 -0500

    Add first component using stateless func

---
+++ src/components/todoContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TodoContainer = () => (
+  <div>
+    <h1>Hello from Create React App</h1>
+    <p>I am in a React Component!</p>
+  </div>
+);
+
+export default TodoContainer;
