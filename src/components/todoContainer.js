6b8dc120825562cdc951345a3bac26194b65c976
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-20 15:41:52 -0500

    Add list of fixed todos

--- src/components/todoContainer.js
+++ src/components/todoContainer.js
@@ -1,10 +1,40 @@
 import React from 'react';
 
-const TodoContainer = () => (
-  <div>
-    <h1>Hello from Create React App</h1>
-    <p>I am in a React Component!</p>
-  </div>
-);
+class TodoContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          title: 'Learn React',
+          completed: false,
+        },
+        {
+          id: 2,
+          title: 'Learn Redux',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Learn React-Redux',
+          completed: false,
+        },
+      ],
+    };
+  }
+
+  render() {
+    const { todos } = this.state;
+    console.log(todos);
+    return (
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    );
+  }
+}
 
 export default TodoContainer;
