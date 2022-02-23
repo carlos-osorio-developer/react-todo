a7864e533fb6527e2621fe05f34c06990d987fd9
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-23 18:57:05 -0500

    Add hamburguer-like menu

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Outlet, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const links = [
     {
       id: 1,
@@ -14,13 +16,28 @@ const Navbar = () => {
       text: 'About',
     },
   ];
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
       <nav className="navBar">
-        <ul>
+        <input
+          type="button"
+          value={isOpen ? 'Close' : 'Open'}
+          onClick={toggleMenu}
+        />
+        <ul className={`menuNav ${isOpen ? 'showMenu' : ''}`}>
           {links.map((link) => (
             <li key={link.id}>
-              <NavLink to={link.path}>{link.text}</NavLink>
+              <NavLink
+                to={link.path}
+                onClick={toggleMenu}
+              >
+                {link.text}
+              </NavLink>
             </li>
           ))}
         </ul>
