b278992e3a6f408b66eccb1a0e1e7e6dc6b72d40
carlos-osorio-developer <osorio.uis@gmail.com>
2022-02-24 16:07:53 -0500

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from "react-router-dom"
+import { HashRouter as Router } from 'react-router-dom';
 import TodoContainer from './components/todoContainer';
 import './App.css';
 
